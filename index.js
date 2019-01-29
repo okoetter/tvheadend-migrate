7f2c29abc572414fc254e96a0f17c7f08ebb6f3a
Oliver Kötter <oliver@koetter.cc>
2019-01-29 22:33:10 +0100

    Add getFinishedRecordings() to read all files from API

---
+++ index.js
@@ -0,0 +1,22 @@
+const
+  configuration = require("./configuration.json"),
+  request = require("request-promise");
+
+const getFinishedRecordings = async () => {
+  try {
+    const response = await request(`http://${configuration.tvheadend_login}:${configuration.tvheadend_password}@${configuration.tvheadend_server}:${configuration.tvheadend_port}/api/dvr/entry/grid_finished?limit=1000000`);
+    return Promise.resolve(response);
+  }
+  catch (error) {
+    console.error("Error contacting the tvheadend API. Check your configuration.json file.");
+    return Promise.reject(error);
+  }
+};
+
+const main = async () => {
+  const result = await getFinishedRecordings();
+  const files = JSON.parse(result);
+  console.log("OK");
+};
+
+main();
