839a284e4eb68f58ad6845359e412a8a4d8fde5b
Oliver Kötter <oliver@koetter.cc>
2020-05-18 22:09:44 +0200

    Use moment js to calculate times

--- tvh-import.js
+++ tvh-import.js
@@ -1,11 +1,12 @@
 // beginning with tvheadend 4.3 you need to set authentication mode to "plain" or "Both plain and digest"
-// in Base settings/HTTP Server SEttings (Expert view level only)
+// in Base settings/HTTP Server Settings (Expert view level only)
 
 const
   configuration = require("./configuration.json"),
   path = require("path"),
   fs = require("fs"),
-  axios = require("axios");
+  axios = require("axios"),
+  moment = require("moment");
 
 const getFinishedRecordings = async () => {
   try {
@@ -20,10 +21,10 @@ const getFinishedRecordings = async () => {
 };
 
 const getFilesToImport = (tvhFiles) => {
-  const allFiles = fs.readdirSync(configuration.source_video_files_folder, { withFileTypes: true });
+  const allFiles = fs.readdirSync(configuration.script_source_video_files_folder, { withFileTypes: true });
   return allFiles
-    .filter((element) => element.isFile() && element.name.match(configuration.source_regex_video_files)) // only files which name matches config
-    .filter(element => tvhFiles.indexOf(element.name) < 0) // only files that are not already in tvh db
+    .filter((element) => element.isFile() && element.name.match(configuration.script_source_regex_video_files)) // only files which name matches config
+    .filter(element => !tvhFiles.includes(element.name)) // only files that are not already in tvh db
     .map(element => element.name); // return only name property
 };
 
@@ -32,7 +33,7 @@ const getVideoDurations = async (files) => {
   const { getVideoDurationInSeconds } = require("get-video-duration");
 
   for (const filename of files) {
-    const duration = await getVideoDurationInSeconds(path.join(configuration.source_video_files_folder, filename))
+    const duration = await getVideoDurationInSeconds(path.join(configuration.script_source_video_files_folder, filename))
     result.push(Math.round(duration));    
   }
 
@@ -45,7 +46,7 @@ const writeCSV = (filesToImport, durations) => {
     const file = fs.createWriteStream(path.join(__dirname, configuration.csv_file));
 
     // write header
-    file.write("filename\tduration\ttitle\tchannel name\tstart time\tstop time\r\n");
+    file.write("filename\tduration\ttitle\tchannel name\tstart time\r\n");
     for (let i = 0; i < filesToImport.length; i++) {
       file.write(`${filesToImport[i]}\t${durations[i]}\r\n`);
     }
@@ -54,8 +55,27 @@ const writeCSV = (filesToImport, durations) => {
   });
 };
 
-const importTest = async () => {
-  const postdata = { 
+const readCSV = async () => {
+  const fs = require("fs");
+  const readline = require('readline');
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream(path.join(__dirname, configuration.csv_file)),
+    crlfDelay: Infinity
+  });
+  
+  let firstRow = true;
+  let result = [];
+  for await (const line of rl) {
+    if (firstRow) firstRow = false; // ignore first row
+    else result.push(line);
+  }
+
+  return result;
+};
+
+const sendToApi = async (tvhFiles) => {
+  /*const postdata = { 
     // "enabled": true, 
     "start": 1509000000, 
     "stop":  1509003600, 
@@ -64,17 +84,62 @@ const importTest = async () => {
     // "subtitle": { "ger": "filename: my video" }, 
     // "description": { "ger": "my description" }, 
     // "comment": "added by tvh_addfile.py", 
-    "files": [ { "filename": "/recordings/das-aktuelle-sportstudio-ZDF-HD2020-04-04_23-00.ts" } ] 
-  };
+    "files": [ { "filename": "/recordings/Tagesschau - --Das Erste HD2020-04-1220-00.ts" } ] 
+  };*/
+  let lines = [];
+  try {
+    lines = await readCSV();   
+  }
+  catch (error) {
+    console.error(`Error reading ${configuration.csv_file}.`);
+    console.error(error);
+    return  
+  }
+
+  moment().format(); 
+  //const test = await axios.post(`http://${configuration.tvheadend_login}:${configuration.tvheadend_password}@${configuration.tvheadend_server}:${configuration.tvheadend_port}/api/dvr/entry/remove`, "uuid=3e3710a0c612d9a9325d1e2fb389a2fe");
 
   try {
-    const response = await axios.post(`http://${configuration.tvheadend_login}:${configuration.tvheadend_password}@${configuration.tvheadend_server}:${configuration.tvheadend_port}/api/dvr/entry/create`, `conf=${JSON.stringify(postdata)}`);
-    return Promise.resolve(response.data);
+    for (const line of lines) {
+      const parts = line.split("\t");
+      if (parts.length >= 5) {
+        if (tvhFiles.includes(parts[0])) continue;
+        const filename = path.join(configuration.tvheadend_recordings_folder, parts[0]);
+        if (fs.existsSync(path.join(configuration.script_source_video_files_folder, parts[0]))) {
+          const startTime = moment(parts[4]);
+          const endTime = startTime
+            .clone()
+            .add(Number(parts[1]), "seconds")
+            .subtract(configuration.pre_recording_padding_minutes, "minutes")
+            .subtract(configuration.post_recording_padding_minutes, "minutes");
+          
+          const postdata = {
+            "start": startTime.unix(),
+            "start_extra": configuration.pre_recording_padding_minutes,
+            "stop": endTime.unix(),
+            "stop_extra": configuration.post_recording_padding_minutes,
+            "channelname": parts[3],
+            "title": {},
+            "files": [ { "filename": filename }]
+          };
+          postdata.title[configuration.country] = parts[2];
+
+          const response = await axios.post(`http://${configuration.tvheadend_login}:${configuration.tvheadend_password}@${configuration.tvheadend_server}:${configuration.tvheadend_port}/api/dvr/entry/create`, `conf=${JSON.stringify(postdata)}`);
+          if (response.status !== 200) {
+            console.error(`Video rejected from API: ${parts[0]}`);
+            console.error(response.statusText);
+          }
+        }
+        else {
+          console.error(`Video file not found: ${parts[0]}`);
+        }
+      }
+    }
   }
   catch (error) {
     console.error("Error contacting the tvheadend API. Check your configuration.json file.");
     console.error(error);
-    return Promise.reject(error);
+    console.error(error.config.data)
   }
 };
 
@@ -85,7 +150,7 @@ const importTest = async () => {
   
   // if csv file exists then import
   if (fs.existsSync(path.join(__dirname, configuration.csv_file)))
-    await importTest();
+    await sendToApi(tvhFiles);
   else { // create csv file
     const durations = await getVideoDurations(filesToImport);
     await writeCSV(filesToImport, durations);
